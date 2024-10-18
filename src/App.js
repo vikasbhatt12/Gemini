d507c1aadb1788bfae322e1830d67cdc55cf1543
vikasbhatt12 <vikasbhatt12082003@gmail.com>
2024-10-18 17:49:33 +0530

    Complete UI implementation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Sidebar from './Components/Sidebar';
+import Main from './Components/Main';
 
 function App() {
   return (
     <>
       <Sidebar/>
+      <Main/>
+      
     </>
   );
 }
