7b8ee7c040901ee0fdf879826e214f4faf1fd153
vikasbhatt12 <vikasbhatt12082003@gmail.com>
2024-10-18 17:57:13 +0530

    Added generative AI functionality to the project

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,63 +1,91 @@
-import React from 'react'
-import './Main.css'
-import { assets } from '../assets/assets'
+import React, { useContext } from "react";
+import "./Main.css";
+import { assets } from "../assets/assets";
+import { Context } from "../Context/Context";
 const Main = () => {
-
-    
+  const {
+    onSent,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    setInput,
+    input,
+  } = useContext(Context);
   return (
-    <div className='main'>
-        <div className='nav'>
-            <p>Gemini</p>
-            <img src={assets.user_icon} alt=''/> 
+    <div className="main">
+      <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt="" />
+      </div>
+      <div className="main-container">
+        {!showResult ? (
+          <>
+            <div className="greet">
+              <p>
+                <span> Hello, dev.</span>
+              </p>
+              <p>How can i help you today?</p>
+            </div>
+            <div className="cards">
+              <div className="card">
+                <p>Suggest beautiful places to see on an upcoming road trip</p>
+                <img src={assets.compass_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Briefly summarize this concept: urban planning</p>
+                <img src={assets.bulb_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Brainstorm team bonding activities for our work retreat</p>
+                <img src={assets.message_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Improve the readability of the following code</p>
+                <img src={assets.code_icon} alt="" />
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="result">
+            <div className="result-title">
+              <img src={assets.user_icon} alt="" />
+              <p>{recentPrompt}</p>
+            </div>
+            <div className="result-data">
+              <img src={assets.gemini_icon} alt="" />
+              {/* <p dangerouslySetInnerHTML={{ __html:resultData }}></p> */}
+              
+              <p>{resultData}</p>
+            </div>
+          </div>
+        )}
+
+        <div className="main-bottom">
+          <div className="search-box">
+            <input
+              onChange={(e) => setInput(e.target.value)}
+              value={input}
+              type="text"
+              placeholder="Enter a prompt here"
+            />
+            <div>
+              <img src={assets.gallery_icon} alt="" />
+              <img src={assets.mic_icon} alt="" />
+              <img
+                onClick={() => onSent(input)}
+                src={assets.send_icon}
+                alt=""
+              />
+            </div>
+          </div>
+          <p className="bottom-info">
+            Gemini can make mistakes, so double-check it
+          </p>
         </div>
-        <div className='main-container'>
-            <div className='greet'>
-                <p><span> Hello, dev.</span></p>
-                <p>How can i help you today?</p>
-
-            </div>
-            <div className='cards'>
-                <div className='card'>
-                    <p>Suggest beautiful places to see on an upcoming road trip</p>
-                    <img src={assets.compass_icon} alt=''/>
-
-                </div>
-                <div className='card'>
-                    <p>Briefly summarize this concept: urban planning</p>
-                    <img src={assets.bulb_icon} alt=''/>
-
-                </div>
-                <div className='card'>
-                    <p>Brainstorm team bonding activities for our work retreat</p>
-                    <img src={assets.message_icon} alt=''/>
-
-                </div>
-                <div className='card'>
-                    <p>Improve the readability of the following code</p>
-                    <img src={assets.code_icon} alt=''/>
-
-                </div>
-
-            </div>
-
-            <div className="main-bottom">
-                <div className="search-box">
-                     <input type="text" placeholder='Enter a prompt here' />
-                         <div>
-                             <img src={assets.gallery_icon} alt="" />
-                             <img src={assets.mic_icon} alt="" />
-                             <img src={assets.send_icon} alt="" />
-                        </div>
-                </div>
-               <p className="bottom-info">
-                  Gemini can make mistakes, so double-check it
-               </p>
-            </div>
-
-        </div>
-      
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
